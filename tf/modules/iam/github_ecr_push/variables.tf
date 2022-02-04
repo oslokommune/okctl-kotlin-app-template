b9a0c61ac90236ec222d4fd531d8eae97d70fcd1
Håvard Eide <haavard.eide@gmail.com>
2022-02-04 08:40:10 +0100

    KM544 - add terraform for setting up OIDC access to ECS (#2)
    Terraform setup for OIDC and updated github workflow to use the new OIDC connection between github and AWS to push to ECS

---
+++ tf/modules/iam/github_ecr_push/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type        = string
+  description = "Used for resource creation. Also used for github/ecr repo names if they are not overriden."
+}
+
+variable "oidc_provider_arn" {
+  type = string
+}
+
+variable "github_account" {
+  type        = string
+  description = "Owner account of github repo"
+  default     = "oslokommune"
+}
+
+variable "github_repo" {
+  type        = string
+  description = "Override value for github repo name"
+  default     = ""
+}
+
+variable "ecr_repo" {
+  type        = string
+  description = "Override value for ECR repo name"
+  default     = ""
+}
+
+variable "tags" {
+  type    = map(any)
+  default = {}
+}
